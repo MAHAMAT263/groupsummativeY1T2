c2c8c4b27ae2c0c91467f9b205314cbde226f44a
pkihiu <p.kihiu@alustudent.com>
2023-06-25 13:16:38 -0700

    main

---
+++ move-to_directory.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+directory_name="negpod_id-ql"
+mkdir "$directory_name"
+mv main.sh students-list_0333.txt select-emails.sh student-emails.txt "$directory_name"
+echo "Files moved to $directory_name directory."
